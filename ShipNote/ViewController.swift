e72aa9d6fb3141339a8a2c4c9425c9ee5fcfc3af
jenny71900 <38311348+jenny71900@users.noreply.github.com>
2018-06-20 15:30:47 +0800

    Initial Commit

---
+++ ShipNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShipNote
+//
+//  Created by User03 on 2018/6/20.
+//  Copyright © 2018年 User03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
